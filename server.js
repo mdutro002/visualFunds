dce21811c1598c42b42b7a58dd055acade59c75a
Madeline Dutro <dutro.madeline@gmail.com>
2020-08-26 01:03:28 -0400

    debug

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-PORT = 3000;
+PORT = 4520;
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,'app/index.html'));
