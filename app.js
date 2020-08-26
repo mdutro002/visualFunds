9c9eb18b2f00b2f7c71d3e6ccbee3ba7312e5f55
Madeline Dutro <dutro.madeline@gmail.com>
2020-08-25 22:48:18 -0400

    adds skeleton

---
+++ app.js
@@ -0,0 +1,8 @@
+//CALCULATOR FUNCTION
+
+const calcInterest = (principle, interestrate, time) => {
+  //A = P(1 + r/n)^(nt)
+  //Pemdas operations:
+  //divide the annual interest rate (r) by the number of times the interest is componded (n)
+  //
+}
