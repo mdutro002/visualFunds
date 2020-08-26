6a1afbd9d443f0ebcc14f196699932c6ab998e7a
Madeline Dutro <dutro.madeline@gmail.com>
2020-08-26 08:28:30 -0400

    reduces file connections to keep things simple/ avoid debugging

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 
 PORT = process.env.PORT || 4520;
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,'app/index.html'));
