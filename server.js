ca3d44c72b82148f0d80eec8455585cec6b24e62
Madeline Dutro <dutro.madeline@gmail.com>
2020-08-26 00:24:28 -0400

    finally renders regular html

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 
+PORT = 3000;
+
 const calcInterest = (principle, interestrate, time) => {
   //A = P(1 + r/n)^(nt)
   //Pemdas operations:
   //divide the annual interest rate (r) by the number of times the interest is componded (n)
   //
 }
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+})
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
